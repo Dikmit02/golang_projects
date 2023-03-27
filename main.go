b3dae24a279b1481b6aec9a8b697474c20a5411d
Narayan K D <narayanK@proactiveforher.com>
2023-03-28 01:16:28 +0530

    ADD: functions

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// diksh := "fuuf" // error
+// var kanika ="fjn"  //
+
+func main(){
+	
+	//  var card string="Ace of Spades"
+
+
+	// card := "Diamond"
+	// card = "Ace of heart"
+	// fmt.Println(card)
+
+
+	card2 :=pp()
+	fmt.Println(card2)
+
+	card3 :=inttt()
+	fmt.Println(card3)
+
+	printState()
+
+}
+
+func  pp() string{
+	return "Five of diamonds"
+}
+
+func  inttt() int{
+	return 3
+}
+
