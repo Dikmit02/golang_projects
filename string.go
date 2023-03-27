706ed559e5b032af20f12b0007a05171e50982b2
Narayan K D <narayanK@proactiveforher.com>
2023-03-28 00:48:12 +0530

    ADD: Strings

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// diksh := "fuuf" // error
+// var kanika ="fjn"  // works
+
+func main(){
+	
+	//  var card string="Ace of Spades"
+	card := "Diamond"
+	card = "Ace of heart"
+	fmt.Println(card)
+}
